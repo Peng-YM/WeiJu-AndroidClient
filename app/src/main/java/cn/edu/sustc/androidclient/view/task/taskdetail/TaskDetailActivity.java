634fd4a9cb38c0e536b7b949d6ac032ae69c22ee
Peng-YM <1048217874pengym@gmail.com>
2018-05-12 23:53:36 +0800

    Fix task detail

--- app/src/main/java/cn/edu/sustc/androidclient/view/task/taskdetail/TaskDetailActivity.java
+++ app/src/main/java/cn/edu/sustc/androidclient/view/task/taskdetail/TaskDetailActivity.java
@@ -2,7 +2,9 @@ package cn.edu.sustc.androidclient.view.task.taskdetail;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.zzhoujay.richtext.RichText;
 
