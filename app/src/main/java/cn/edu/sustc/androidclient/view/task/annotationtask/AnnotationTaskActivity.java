9246c97e63e25142abee505cba7ed2f36b4d87c6
Alin Zhang  <397670339@qq.com>
2018-05-22 22:30:31 +0800

    Update annotation task

--- app/src/main/java/cn/edu/sustc/androidclient/view/task/annotationtask/AnnotationTaskActivity.java
+++ app/src/main/java/cn/edu/sustc/androidclient/view/task/annotationtask/AnnotationTaskActivity.java
@@ -1,15 +1,27 @@
 package cn.edu.sustc.androidclient.view.task.annotationtask;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.edu.sustc.androidclient.R;
 import cn.edu.sustc.androidclient.common.base.BaseActivity;
 import cn.edu.sustc.androidclient.databinding.ActivityAnnotationTaskBinding;
 
 public class AnnotationTaskActivity extends BaseActivity<AnnotationTaskViewModel, ActivityAnnotationTaskBinding> {
+    @BindView(R.id.add_tag)
+    Button add_tag;
+    @BindView(R.id.next_step)
+    Button next_step;
     private AnnotationTaskViewModel viewModel;
     private ActivityAnnotationTaskBinding binding;
     private AnnotateImageView annotateImageView;
@@ -31,18 +43,64 @@ public class AnnotationTaskActivity extends BaseActivity<AnnotationTaskViewModel
 
         annotateImageView = binding.annotateImageView;
         annotateImageView.init(BitmapFactory.decodeResource(getResources(), R.drawable.cover));
-        binding.undoButton.setOnClickListener(view -> {
-            annotateImageView.undo();
+
+        ButterKnife.bind(this);
+        add_tag.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                popup_tags();
+            }
         });
-        binding.clearButton.setOnClickListener(view -> {
-            annotateImageView.clear();
+        next_step.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                // next image
+            }
         });
-        binding.modeButton.setOnClickListener(view -> {
-            AnnotateImageView.Mode mode =
-                    annotateImageView.getMode() == AnnotateImageView.Mode.EDIT
-                            ? AnnotateImageView.Mode.SELECT : AnnotateImageView.Mode.EDIT;
-            annotateImageView.setMode(mode);
+
+//        binding.undoButton.setOnClickListener(view -> {
+//            annotateImageView.undo();
+//        });
+//        binding.clearButton.setOnClickListener(view -> {
+//            annotateImageView.clear();
+//        });
+//        binding.modeButton.setOnClickListener(view -> {
+//            AnnotateImageView.Mode mode =
+//                    annotateImageView.getMode() == AnnotateImageView.Mode.EDIT
+//                            ? AnnotateImageView.Mode.SELECT : AnnotateImageView.Mode.EDIT;
+//            annotateImageView.setMode(mode);
+//        });
+
+    }
+
+    private void popup_tags(){
+        PopupMenu popup = new PopupMenu(AnnotationTaskActivity.this, add_tag);
+        Menu menu = popup.getMenu();
+
+        // 此处应该用for循环加载tag
+        // 一个粗暴的test
+        menu.add(Menu.NONE, Menu.FIRST + 0, 0, "tag1");
+        menu.add(Menu.NONE, Menu.FIRST + 1, 1, "tag2");
+        menu.add(Menu.NONE, Menu.FIRST + 2, 2, "tag3");
+        menu.add(Menu.NONE, Menu.FIRST + 3, 3, "tag4");
+        // menu的item点击事件
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case Menu.FIRST + 0:
+                        // 对标签模式的设置（颜色等）
+                        break;
+                    case Menu.FIRST + 1:
+                        break;
+                    case Menu.FIRST + 2:
+                        break;
+                    case Menu.FIRST + 3:
+                        break;
+                }
+                return false;
+            }
         });
+
+        popup.show(); //showing popup menu
     }
 
     @Override
--- app/src/main/java/cn/edu/sustc/androidclient/view/task/annotationtask/TagEditorActivity.java
+++ app/src/main/java/cn/edu/sustc/androidclient/view/task/annotationtask/TagEditorActivity.java
@@ -1,18 +1,26 @@
 package cn.edu.sustc.androidclient.view.task.annotationtask;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
+import cn.edu.sustc.androidclient.R;
 import cn.edu.sustc.androidclient.common.base.BaseActivity;
 import cn.edu.sustc.androidclient.databinding.ActivityTagEditorBinding;
 
 public class TagEditorActivity extends BaseActivity<AnnotationTaskViewModel, ActivityTagEditorBinding> {
+
     @Override
     protected Class<AnnotationTaskViewModel> getViewModel() {
         return null;
     }
 
     @Override
-    protected void onCreate(Bundle instance, AnnotationTaskViewModel viewModel, ActivityTagEditorBinding binding) {
+    protected void onCreate(Bundle savedInstanceState, AnnotationTaskViewModel viewModel, ActivityTagEditorBinding binding) {
 
     }
 
@@ -20,4 +28,5 @@ public class TagEditorActivity extends BaseActivity<AnnotationTaskViewModel, Act
     protected int getLayoutResId() {
         return 0;
     }
+
 }
